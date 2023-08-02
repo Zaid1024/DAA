15c4fb8706aeb2e25ba7ab2c49921c1ccc5c5291
Zaid1024 <141013542+Zaid1024@users.noreply.github.com>
2023-08-02 21:29:58 +0530

    Update djk.c

--- 15/djk.c
+++ 15/djk.c
@@ -1,75 +1,66 @@
-#include <limits.h>
-#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int minDistance(int dist[], bool sptSet[], int V) {
-    int min = INT_MAX, min_index;
-
-    for (int v = 0; v < V; v++)
-        if (sptSet[v] == false && dist[v] <= min)
-            min = dist[v], min_index = v;
-
-    return min_index;
-}
-
-void printSolution(int dist[], int V) {
-    printf("Vertex \t\t Distance from Source\n");
-    for (int i = 0; i < V; i++)
-        printf("%d \t\t\t\t %d\n", i, dist[i]);
-}
-
-void dijkstra(int **graph, int src, int V) {
-    int *dist = (int *)malloc(V * sizeof(int));
-    bool *sptSet = (bool *)malloc(V * sizeof(bool));
-
-    for (int i = 0; i < V; i++)
-        dist[i] = INT_MAX, sptSet[i] = false;
-
-    dist[src] = 0;
-
-    for (int count = 0; count < V - 1; count++) {
-        int u = minDistance(dist, sptSet, V);
-        sptSet[u] = true;
-
-        for (int v = 0; v < V; v++)
-            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
-                dist[v] = dist[u] + graph[u][v];
+void dijkstra(int n, int cost[10][10], int s, int dist[10])
+{
+    int i, v, count = 1, min, visited[10];
+	
+    for (i = 1; i <= n; i++)
+    {
+        visited[i] = 0;
+        dist[i] = cost[s][i]; 
     }
 
-    printSolution(dist, V);
+    visited[s] = 1;
+    dist[s] = 0; 
 
-    free(dist);
-    free(sptSet);
-}
+    while (count < n)
+    {
+        min = 999;
+			
+        for (i = 1; i <= n; i++)
+        {
+            if (dist[i] < min && !visited[i])
+            {
+                min = dist[i];
+                v = i;
+            }
+        }
+        visited[v] = 1;
+        count++;
 
-int main() {
-    int V;
-
-    printf("Enter the number of vertices: ");
-    scanf("%d", &V);
-
-    int **graph = (int **)malloc(V * sizeof(int *));
-    for (int i = 0; i < V; i++) {
-        graph[i] = (int *)malloc(V * sizeof(int));
-    }
-
-    printf("Enter the adjacency matrix (%dx%d):\n", V, V);
-    for (int i = 0; i < V; i++) {
-        for (int j = 0; j < V; j++) {
-            scanf("%d", &graph[i][j]);
+        for (i = 1; i <= n; i++)
+        {
+            if (dist[i] > dist[v] + cost[v][i])
+                dist[i] = dist[v] + cost[v][i];
         }
     }
-int x;
-		printf("Enter the vertex to be the source :");
-	scanf("%d",&x);
-    printf("\nShortest distances from source vertex %d:\n",x);
-    dijkstra(graph, x, V);
+}
 
-    for (int i = 0; i < V; i++) {
-        free(graph[i]);
-    }
-    free(graph);
+void main()
+{
+  int i, j, n, s, cost[10][10], dist[10];
+	
+  printf("Enter the total number of nodes: ");
+  scanf("%d", &n);
 
-    return 0;
+  printf("Read the cost matrix:(insert '0' for infinity)\n");
+  for (i = 1; i <= n; i++)
+  {
+    for (j = 1; j <= n; j++)
+    { 
+			scanf("%d", &cost[i][j]);
+    	  if (cost[i][j] == 0)
+  	       cost[i][j] = 999;
+      }
+  }
+
+  printf("Enter the source vertex: ");
+  scanf("%d", &s);
+  dijkstra(n, cost, s, dist);
+
+  printf("Shortest path from %d is:\n", s);
+  for (i = 1; i <= n; i++) 
+		{
+	   printf("%d -> %d = %d\n", s, i, dist[i]); 
+  }   
 }
